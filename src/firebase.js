a5e5a0c5485fdafe43d15336a1a340d7b470fa55
Leopoldo Romero <nicoromerom87@gmail.com>
2020-12-10 14:56:36 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCU9TWb1C2dtTyukEDA_HFkkhqM8ItQVg8",
+  authDomain: "whatsapp-clone-c57ca.firebaseapp.com",
+  projectId: "whatsapp-clone-c57ca",
+  storageBucket: "whatsapp-clone-c57ca.appspot.com",
+  messagingSenderId: "564855095931",
+  appId: "1:564855095931:web:6a66942c4b3d9970de18de",
+  measurementId: "G-EDHBJR1WJT"
+};
